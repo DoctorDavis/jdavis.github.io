ddd44df4cfcd05fb67378dadb41eba73dafca308
Jim Davis <james.davis7@dcmail.ca>
2025-03-09 15:26:55 -0400

    Final Code
    All code has been entered and Corrected numerous times...lol

--- Assignment 4/Part1/main.js
+++ Assignment 4/Part1/main.js
@@ -13,10 +13,9 @@ function randomValueFromArray(array){
     const random = Math.floor(Math.random()*array.length);
     return array[random];
 }
+
 // create storyText and 3 Arrays
-const storyText = "It was 94 fahrenheit outside, so :insertX: went for a walk. When they got to :inserty:, 
-                    they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but 
-                    was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+const storyText = "It was 94 fahrenheit outside, so :insertX: went for a walk. When they got to :insertY:, they stared in horror for a few moments, then :insertZ:. Bob saw the whole thing, but was not surprised — :insertX: weighs 300 pounds, and it was a hot day.";
 
 const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 
@@ -45,10 +44,15 @@ function result() {
   }
 
     if(document.getElementById("uk").checked) {
-        const weight = Math.round(300);
-        const temperature =  Math.round(94);
+        const weight = Math.round(300/14);
+        const temperature = Math.round((94-32)*(5/9));
+        newStory = newStory
+                    .replace("300", weight)
+                    .replace(" pounds", " stone")
+                    .replace("94", temperature)
+                    .replace(" fahrenheit", " centigrade");
   }
 
-    story.textContent = ;
+    story.textContent = newStory;
     story.style.visibility = 'visible';
 }
