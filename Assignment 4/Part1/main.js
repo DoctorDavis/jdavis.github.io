8b639256c25971025c99bcda66b20de39b781407
Jim Davis <james.davis7@dcmail.ca>
2025-03-09 14:11:32 -0400

    Update main.js
    added newStory = to storyText
    added xItem, yItem, and zItem = each one equal to random element from its respective array

--- Assignment 4/Part1/main.js
+++ Assignment 4/Part1/main.js
@@ -22,3 +22,25 @@ const insertY = ["the soup kitchen", "Disneyland","the White House"];
 
 const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"]; 
 
+
+// randomizer which is a click event
+randomize.addEventListener('click', result);
+
+function result() {
+    let newStory = storyText;
+    const xItem = randomValueFromArray(insertX);
+    const yItem = randomValueFromArray(insertY);
+    const zItem = randomValueFromArray(insertZ);
+
+    if(customName.value !== '') {
+        const name = customName.value;
+  }
+
+    if(document.getElementById("uk").checked) {
+        const weight = Math.round(300);
+        const temperature =  Math.round(94);
+  }
+
+    story.textContent = ;
+    story.style.visibility = 'visible';
+}
