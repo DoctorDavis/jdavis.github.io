ec6f2417794f0ea9cba99fbb14b89fb62292f6f2
Jim Davis <james.davis7@dcmail.ca>
2025-03-18 10:30:15 -0400

    Start of Assign 4 - Part 4
    Created 3 files, with data from github
    main.js, style.css, and index.html

---
+++ Assignment 4/Part4/main.js
@@ -0,0 +1,26 @@
+/*
+	Name: Jim Davis
+	File: main.js
+	Date: 18 March 2025
+	Description: Assingment 4 - Part 4 -Adding Bounding Balls Features.
+	*/
+
+// setup canvas
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+// function to generate random number
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// function to generate random color
+
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
